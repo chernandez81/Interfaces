2ef5a747ab0a887f279226f2772c229edf81b1b9
Victor <victor.morenojimenez@amdocs.com>
2020-11-14 20:39:19 -0600

    commit upload two
    commit upload two

---
+++ SuperMarketGDL_without_unity/SuperMarketGDL.LogicLayer/Classes/ProviderBusinessLogic.cs
@@ -0,0 +1,19 @@
+﻿using SuperMarketGDL.BaseClasses.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperMarketGDL.ServiceConnection.Classes;
+using SuperMarketGDL.BaseClasses.Interfaces;
+
+namespace SuperMarketGDL.BusinessLogic.Classes
+{
+    public class ProviderBusinessLogic
+    {
+        public static IRepositoryToDbProviders GetProviderEntity(ProviderConnectionType provConnection)
+        {
+            return ProviderService.GetProviderEntity(provConnection);
+        }
+    }
+}
---
+++ SuperMarketGDL_without_unity/SuperMarketGDL.ServiceConnection/Classes/ProviderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SuperMarketGDL.BaseClasses.Enums;
+using SuperMarketGDL.BaseClasses.Interfaces;
+using SuperMarketGDL.DataAccess.Classes;
+
+namespace SuperMarketGDL.ServiceConnection.Classes
+{
+    public class ProviderService
+    {
+        public static IRepositoryToDbProviders GetProviderEntity(ProviderConnectionType provConnection)
+        {
+            return ProviderDispatcher.GetProviderEntity(provConnection);
+        }
+    }
+}
